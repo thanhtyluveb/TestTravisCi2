4cc45e093b0d3b9fe51a226b62ab96b27a32ded3
Ngoc <thanhtyluven@gmail.com>
2019-04-01 13:21:30 +0700

    config yml

--- app/src/main/java/com/example/testtravisci_2/MainActivity.kt
+++ app/src/main/java/com/example/testtravisci_2/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText(this,"ok",Toast.LENGTH_LONG).show()
+        Toast.makeText(this,"ok ok",Toast.LENGTH_LONG).show()
     }
 }
